bccae84b186672898b86ff2b56fc6d283dc3cb37
Jason Sabal <jason.sabal.861@my.csun.edu>
2021-11-04 16:04:48 -0700

    Update navbar styling

--- src/script.js
+++ src/script.js
@@ -1,11 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const hamburgerExit = document.querySelector(".hamburger-exit");
-const navMenu = document.querySelector(".nav__menu");
+const navMenu = document.querySelector(".nav__menu__container");
 
 const toggleDrawer = () => {
-    hamburger.classList.toggle("hide");
-    hamburgerExit.classList.toggle("show");
-    navMenu.classList.toggle("show");    
+    navMenu.classList.toggle("active");    
 }
 
 hamburger.addEventListener("click", toggleDrawer);
